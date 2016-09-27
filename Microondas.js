72408679a84311420713fbc6da5baf08d66a6b24
Angela Rangel <angelarangel91@gmail.com>
2016-09-27 16:11:31 -0430

    subiendo actividad

---
+++ Microondas.js
@@ -0,0 +1,20 @@
+function descongelar (){
+ var tipo_comida=prompt("elija el tipo de comida");
+ alert ("usted desea descongelar: " + tipo_comida);
+
+}
+
+function cocinar(){
+	var cocina=prompt("Elija qué va a cocinar");
+	var tiempo=prompt("indique tiempo de cocción");
+	alert("usted desea cocinar: " +cocina +" en:" +tiempo)
+}
+
+
+function  preparacion() {
+ var descongelar=prompt("elija el tipo de comida a descongelar");
+ var gratinar=prompt("Gratinado");
+ var duracion=prompt("indique tiempo");
+ alert ("usted desea descongelar: " + descongelar + " gratinar: " + gratinar + " en: " + duracion);
+ 
+} 
